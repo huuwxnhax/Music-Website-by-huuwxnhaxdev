8d112a0d3651acd6d3e52c7940fb9ea96b77c9e3
huuwxnhax <111059487+huuwxnhax@users.noreply.github.com>
2022-08-11 21:15:21 +0700

    Load source code

--- assets/js/mv.js
+++ assets/js/mv.js
@@ -1,20 +1,20 @@
 var mvLists = [
     {
-        background: "./assets/img/mv/mv_1.png",
+        background: "./assets/img/mv/mv_24gio.png",
         title: "24H",
         subtitle: "LyLy, Magazine",
         thumb: "./assets/img/thumbs/lyly.jpg"
     },
 
     {
-        background: "./assets/img/mv/mv_2.png",
+        background: "./assets/img/mv/mv_dalab.png",
         title: "Nước Mắt Em Lau Bằng Tình Yêu Mới",
         subtitle: "Da LAB, Tốc Tiên",
         thumb: "./assets/img/thumbs/toctien.webp"
     },
 
     {
-        background: "./assets/img/mv/mv_3.png",
+        background: "./assets/img/mv/mv_amee.png",
         title: "Sao anh chưa về",
         subtitle: "AMEE, Ricky Star",
         thumb: "./assets/img/thumbs/amee.webp"
